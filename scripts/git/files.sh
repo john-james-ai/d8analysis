ec9f9c29734940e2e525f378918cfde08265b603
John James <john.james.ai.studio@gmail.com>
2023-06-06 23:18:18 -0400

    distribution done, deleted lgfile

---
+++ scripts/git/files.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Enter Project Name in Workspace Settings                                            #
+# Version    : 0.1.19                                                                              #
+# Python     : 3.10.10                                                                             #
+# Filename   : /scripts/git/files.sh                                                               #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : Enter URL in Workspace Settings                                                     #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Tuesday June 6th 2023 11:12:53 pm                                                   #
+# Modified   : Tuesday June 6th 2023 11:12:59 pm                                                   #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+git rev-list --objects --all |
+  git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
+  sed -n 's/^blob //p' |
+  sort --numeric-sort --key=2 |
+  cut -c 1-12,41- |
+  $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
