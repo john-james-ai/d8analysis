e7dcc53756d897e2ff1ff35fbc5e02ab03cc0f20
John James <john.james.ai.studio@gmail.com>
2023-08-21 14:38:38 -0400

    autowire n.0

--- d8analysis/__init__.py
+++ d8analysis/__init__.py
@@ -11,11 +11,17 @@
 # URL        : https://github.com/john-james-ai/d8analysis                                         #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Friday May 26th 2023 11:59:46 pm                                                    #
-# Modified   : Monday August 21st 2023 01:48:53 pm                                                 #
+# Modified   : Monday August 21st 2023 02:37:24 pm                                                 #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2023 John James                                                                 #
 # ================================================================================================ #
 import logging
+from d8analysis.container import D8AnalysisContainer
 
+# ------------------------------------------------------------------------------------------------ #
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+# ------------------------------------------------------------------------------------------------ #
+container = D8AnalysisContainer()
+container.init_resources()
+container.wire(packages=["d8analysis"])
